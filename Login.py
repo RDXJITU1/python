3a4086a6009062fe2058672d2ba3c61821313206
Jitendra Das <jitud9902@gmail.com>
2026-06-14 09:20:46 +0530

    first require2

--- Login.py
+++ Login.py
@@ -20,7 +20,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins = ["https://login-form-theta-rust.vercel.app/"],
+    allow_origins = ["https://login-form-theta-rust.vercel.app"],
     allow_credentials=True,
     allow_methods = ["*"],
     allow_headers = ["*"]
