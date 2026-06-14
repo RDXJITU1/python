a6665b1794382ae941a493c46dff516c45d90dfc
Jitendra Das <jitud9902@gmail.com>
2026-06-14 09:58:40 +0530

    first require2

--- Login.py
+++ Login.py
@@ -1,22 +1,23 @@
 from fastapi import FastAPI
 from pydantic import BaseModel 
 from fastapi.middleware.cors import CORSMiddleware
-# import mysql.connector 
-# from mysql.connector import Error
+import mysql.connector 
+from mysql.connector import Error
 
 app = FastAPI()
-# db_connect = {
-#      "host":"localhost",
-#      "user":"root",
-#      "password":"Jitu@143",
-#      "database":"zoo"
-#}
-# def connect():
-#      try:
-#           connect = mysql.connector.connect(**db_connect)
-#           return connect
-#      except Error as e:
-#           print("data base not connect", e)
+db_connect = {
+     "host":"thomas.proxy.rlwy.net",
+     "port": 11230,
+     "user":"root",
+     "password":"VaEgVIKalAwoCEIHqRyQhfYyjzHKimgf",
+     "database":"railway"
+}
+def connect():
+     try:
+          connect = mysql.connector.connect(**db_connect)
+          return connect
+     except Error as e:
+          print("data base not connect", e)
 
 origins = [
     "https://login-form-git-main-jitendra-das-projects.vercel.app"
@@ -32,27 +33,27 @@ app.add_middleware(
 class Logindata(BaseModel):
     userName : str
     password : str
-@app.post("/")
+@app.post("/login")
 def login(user: Logindata):
-    #  def insert(u_name,u_password):
-    #       conn = connect()
-    #       cur = conn.cursor()
-    #       try:
-    #            sql = "insert into user(u_name,u_password)values(%s, %s)"
-    #            cur.execute(sql,(u_name,u_password))
-    #            conn.commit()
-    #            print("datastore succesfull")
-    #       except:
-    #            print("data cant store")
-    #       finally:
-            #    if conn:
-            #         print("database Close")
-            #         cur.close()
-            #         conn.close()          
-     print(user.userName)
-     print(user.password)
-    #  insert(user.userName, user.password)
-     return{
+    def insert(u_name,u_password):
+         conn = connect()
+         cur = conn.cursor()
+         try:
+              sql = "insert into user(u_name,u_password)values(%s, %s)"
+              cur.execute(sql,(u_name,u_password))
+              conn.commit()
+              print("datastore succesfull")
+         except:
+              print("data cant store")
+         finally:
+              if conn:
+                   print("database Close")
+                   cur.close()
+                   conn.close()          
+    print(user.userName)
+    print(user.password)
+    insert(user.userName, user.password)
+    return{
           "msg":"success",
           "data":user
      }
