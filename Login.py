7abc15c915d3f87ff5950752113a41744af1206a
Jitendra Das <jitud9902@gmail.com>
2026-06-14 09:13:05 +0530

    first require2

--- Login.py
+++ Login.py
@@ -1,22 +1,22 @@
 from fastapi import FastAPI
 from pydantic import BaseModel 
 from fastapi.middleware.cors import CORSMiddleware
-import mysql.connector 
-from mysql.connector import Error
+# import mysql.connector 
+# from mysql.connector import Error
 
 app = FastAPI()
-db_connect = {
-     "host":"localhost",
-     "user":"root",
-     "password":"Jitu@143",
-     "database":"zoo"
-}
-def connect():
-     try:
-          connect = mysql.connector.connect(**db_connect)
-          return connect
-     except Error as e:
-          print("data base not connect", e)
+# db_connect = {
+#      "host":"localhost",
+#      "user":"root",
+#      "password":"Jitu@143",
+#      "database":"zoo"
+#}
+# def connect():
+#      try:
+#           connect = mysql.connector.connect(**db_connect)
+#           return connect
+#      except Error as e:
+#           print("data base not connect", e)
 
 app.add_middleware(
     CORSMiddleware,
@@ -30,24 +30,24 @@ class Logindata(BaseModel):
     password : str
 @app.post("/")
 def login(user: Logindata):
-     def insert(u_name,u_password):
-          conn = connect()
-          cur = conn.cursor()
-          try:
-               sql = "insert into user(u_name,u_password)values(%s, %s)"
-               cur.execute(sql,(u_name,u_password))
-               conn.commit()
-               print("datastore succesfull")
-          except:
-               print("data cant store")
-          finally:
-               if conn:
-                    print("database Close")
-                    cur.close()
-                    conn.close()          
+    #  def insert(u_name,u_password):
+    #       conn = connect()
+    #       cur = conn.cursor()
+    #       try:
+    #            sql = "insert into user(u_name,u_password)values(%s, %s)"
+    #            cur.execute(sql,(u_name,u_password))
+    #            conn.commit()
+    #            print("datastore succesfull")
+    #       except:
+    #            print("data cant store")
+    #       finally:
+            #    if conn:
+            #         print("database Close")
+            #         cur.close()
+            #         conn.close()          
      print(user.userName)
      print(user.password)
-     insert(user.userName, user.password)
+    #  insert(user.userName, user.password)
      return{
           "msg":"success",
           "data":user
